ac332acf53cc35c311645ae615c5de0505d54620
Frederik Callin Østern <frederikcallin@gmail.com>
2025-09-24 15:43:45 +0200

    Fix resample function

--- code/methods/resampling.py
+++ code/methods/resampling.py
@@ -7,8 +7,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.utils import resample
 from sklearn.preprocessing import StandardScaler
 
-# train_test_split function from documentation
-
 class BiasVariance:
      def __init__(self,x,y,max_degree = 15,test_size = 0.2,n_boostraps = 100,random_state = 42): 
           self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x.reshape(-1,1), y.reshape(-1,1), test_size=test_size,random_state = random_state)
@@ -17,7 +15,6 @@ class BiasVariance:
           self.MSE = np.zeros(max_degree)
           self.bias = np.zeros(max_degree)
           self.variance = np.zeros(max_degree)
-          #self.rng = np.random.RandomState(121)
 
      def bootstrap(self):
 
@@ -26,7 +23,7 @@ class BiasVariance:
             y_pred = np.zeros((self.y_test.shape[0], self.n_boostraps))
 
             for i in range(self.n_boostraps):
-                x_, y_ = resample(self.x_train, self.y_train,random_state = 121)
+                x_, y_ = resample(self.x_train, self.y_train,random_state = i)
                 y_pred[:, i] = model.fit(x_, y_).predict(self.x_test).ravel()
 
             self.MSE[degree-1] = np.mean(np.mean((self.y_test - y_pred)**2, axis=1, keepdims=True))
