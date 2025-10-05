47c7e36090007b7367e1ebbb533082afceb8d62a
Heine Husdal <heine.elias.husdal@gmail.com>
2025-10-05 12:33:14 +0200

    Renamed general_methods to utils, and removed custom polynomial_features function to instead use sklearn implementation

--- code/utils.py
+++ code/utils.py
@@ -16,17 +16,17 @@ def Ridge_parameters(X, y, lambda_reg = 0.1):
     I = np.eye(pred) # size: (p,p)
     return np.linalg.pinv((X.T @ X)+len(y)*I*lambda_reg) @ X.T @ y
 
-def polynomial_features(x, p, intercept = False):
-    n = len(x)
-    if intercept:
-        X = np.zeros((n,p+1))
-        for i in range(p+1):
-            X[:, i] = x**i
-    else:
-        X = np.zeros((n, p))
-        for i in range(1, p + 1):
-            X[:, i - 1] = x ** i
-    return X
+# def polynomial_features(x, p, intercept = False):
+#     n = len(x)
+#     if intercept:
+#         X = np.zeros((n,p+1))
+#         for i in range(p+1):
+#             X[:, i] = x**i
+#     else:
+#         X = np.zeros((n, p))
+#         for i in range(1, p + 1):
+#             X[:, i - 1] = x ** i
+#     return X
 
 def MSE(y, y_pred):
     return 1/len(y) * np.sum((y-y_pred)**2)
@@ -47,7 +47,7 @@ def MSE_and_R2(x,y,polynomial_degrees,regression_type="OLS",lamb = 0.0):
     mse_values = np.zeros(len(polynomial_degrees))
     r2_values = np.zeros(len(polynomial_degrees))
     for i in range(len(polynomial_degrees)):
-        X =  polynomial_features(x, polynomial_degrees[i])
+        X =  PolynomialFeatures(polynomial_degrees[i], include_bias=False).fit_transform(x.reshape(-1, 1))
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
         theta, scaler = opt_theta(X_train,y_train,regression_type = regression_type, lamb = lamb)
         X_test_s = scaler.transform(X_test)
